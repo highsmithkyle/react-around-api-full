a9d0836a1b4812183b33a06ef88b878f8630ac90
Kyle Highsmith <kyle@Kyles-Air.technicolor.net>
2022-06-28 13:48:28 +0200

    add error and update createUser controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,11 @@ const User = require('../models/user');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const BadRequestError = require('../errors/bad-request-error');
+const ConflictError = require('../errors/conflict-error');
+const NotFoundError = require('../errors/not-found-error');
+const UnauthorizedError = require('../errors/unauthorized-error');
+
 const {
   HTTP_SUCCESS,
   HTTP_CREATED,
@@ -10,23 +15,81 @@ const {
   HTTP_INTERNAL_SERVER_ERROR,
 } = require('../utils/error');
 
-const createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.status(HTTP_CREATED).send({ data: user }))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(HTTP_BAD_REQUEST).send({
-          message: error.message,
-        });
+const createUser = (req, res, next) => {
+  const { name, about, avatar, email, password } = req.body;
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        throw new ConflictError(
+          'The user with the provided email already exists',
+        );
       } else {
-        res
-          .status(HTTP_INTERNAL_SERVER_ERROR)
-          .send({ message: 'An error has occured' });
+        return bcrypt.hash(password, 10);
+      }
+    })
+    .then((hash) =>
+      User.create({
+        name,
+        about,
+        avatar,
+        email,
+        password: hash,
+      }),
+    )
+    .then((data) => res.status(201).send({ data }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Missing or invalid email or password'));
+      } else {
+        next(err);
       }
     });
 };
+
+// const createUser = (req, res, next) => {
+//   const { name, about, avatar, email, password } = req.body;
+
+//   User.findOne({ email })
+//     .then((user) => {
+//       if (user) {
+//         throw new ConflictError('User already exists');
+//       } else {
+//         return bcrypt.hash(password, 10);
+//       }
+//     })
+
+//     .then((hash) =>
+//       User.create({
+//         name,
+//         about,
+//         avatar,
+//         email,
+//         password: hash,
+//       }),
+//     )
+//     .then((data) => res.status(201).send({ data }))
+//     .catch((err) => {
+//       if (err.name === 'ValidationError') {
+//         next(new BadRequestError('Invalid email or password'));
+//       } else {
+//         next(err);
+//       }
+//     });
+
+//   User.create({ name, about, avatar })
+//     .then((user) => res.status(HTTP_CREATED).send({ data: user }))
+//     .catch((error) => {
+//       if (error.name === 'ValidationError') {
+//         res.status(HTTP_BAD_REQUEST).send({
+//           message: error.message,
+//         });
+//       } else {
+//         res
+//           .status(HTTP_INTERNAL_SERVER_ERROR)
+//           .send({ message: 'An error has occured' });
+//       }
+//     });
+// };
 
 const getUsers = (req, res) => {
   User.find({})
